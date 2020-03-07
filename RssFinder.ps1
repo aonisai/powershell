7b09cdf2dbf25d42d51229699b49d0bbdf8f7331
en1gma <seiunryou0602@gmail.com>
2020-03-07 23:48:33 +0900

    add RssFinder

---
+++ RssFinder.ps1
@@ -0,0 +1,90 @@
+﻿Param(
+    [string]$search_word = "indicators*\s*of\s*compromise",
+    [string]$search_word_02 = "ioc",
+    [int]$search_days_arg = 30, # 何日前まで取得するか指定, デフォルトは30日
+    [parameter(mandatory=$true)]$rss_list_arg # 調査rssのリスト(txt), 必須
+)
+
+function Find-Match{
+    If($search_days -le $date){
+        $wc_all = $wc.DownloadString($item.link.Trim())
+        if($wc_all | Select-String -Pattern $search_word){
+            Write-Host "Yes!!"
+           "url:" + $item.link.Trim()
+           "title:" + $item.title
+        }
+        elseif($wc_all | Select-String -Pattern $search_word_02){
+            Write-Host "ioc!!"
+            "url:" + $item.link.Trim()
+            "title:" + $item.title
+        }
+    }
+}
+
+[String]$encoding='UTF-8'
+# 各rssを配列で格納
+[string[]]$rss_list = (Get-Content $rss_list_arg) -as [string[]]
+
+# $search_daysがデフォルトかどうか
+If([string]::IsNullOrEmpty($search_days_arg)){
+    # デフォルトの場合30日前に設定
+}else{
+    # 指定があった場合, 実行日時から計算して何日になるか計算
+    $search_days = (Get-Date).AddDays(-$search_days_arg)
+}
+
+$cli = New-Object System.Net.WebClient
+$cli.Encoding = [System.Text.Encoding]::UTF8
+$wc = New-Object System.Net.WebClient
+$wc.Encoding = [System.Text.Encoding]::UTF8
+
+# rssリストのループ
+foreach($rss in $rss_list){
+    $rss = [xml]$cli.DownloadString($rss)
+    if($rss.rss -ne $null){ # rss=2.0
+        "========================================"
+        "blog_title:" + $rss.rss.channel.title + ""
+        foreach($item in $rss.rss.channel.item){
+            # 日付の比較
+            $date = [DateTime]$item.pubDate
+            Find-Match
+<#            If($search_days -le $date){
+                $wc_all = $wc.DownloadString($item.link.Trim())
+                if($wc_all | Select-String -Pattern $search_word){
+                    Write-Host "Yes!!"
+                    "url:" + $item.link.Trim()
+                    "title:" + $item.title
+                }
+                elseif($wc_all | Select-String -Pattern $search_word_02){
+                    Write-Host "ioc!!"
+                    "url:" + $item.link.Trim()
+                    "title:" + $item.title
+                }
+            } #>
+        }
+    }
+    else{ # rss=1.0
+        "blog_title:" + $rss.RDF.channel.title + ""
+        # rssのitemのループ
+        foreach($item in $rss.RDF.item){
+            # 日付の比較
+            $date = [DateTime]$item.date
+            Find-Match
+<#            If($search_days -le $date){
+                $wc_all = $wc.DownloadString($item.link.Trim())
+                if($wc_all | Select-String -Pattern $search_word){
+                    Write-Host "Yes!!"
+                    "url:" + $item.link.Trim()
+                    "title:" + $item.title
+                }
+                elseif($wc_all | Select-String -Pattern $search_word_02){
+                    Write-Host "ioc!!"
+                    "url:" + $item.link.Trim()
+                    "title:" + $item.title
+
+                }
+            } #>
+        }
+    }
+    "========================================"
+}
