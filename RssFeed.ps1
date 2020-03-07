db59ef178a51bf759b9ce87ff4d9bf94d3662d45
en1gma <seiunryou0602@gmail.com>
2020-03-08 00:15:42 +0900

    Rss Feed test program

---
+++ RssFeed.ps1
@@ -0,0 +1,17 @@
+﻿# Param([String]$url, [String]$encodeing='UTF-8')
+[String]$url = "https://wings.msn.to/contents/rss.php"
+[String]$encoding='UTF-8'
+
+$cli = New-Object System.Net.WebClient
+
+$cli.Encoding = [System.Text.Encoding]::UTF8
+$rss = [xml]$cli.DownloadString($url)
+
+# $doc = [System.Text.Encoding]::GetEncoding($encoding).GetString($cli.DownloadData($url))
+
+"<html><body><ul>"
+foreach($item in $rss.RDF.item){
+    "<li><a href='" + $item.link.Trim() + "'>"
+    [String]$item.title + "</a></li>"
+}
+"</ul></body></html>"
