32b181a85ecd5c4abd4e3b6f0d41a4ae74c4896c
en1gma <seiunryou0602@gmail.com>
2020-03-12 16:38:40 +0900

    program for qiita

---
+++ RssFinderforQiita.ps1
@@ -0,0 +1,57 @@
+﻿Param(
+    [string]$search_word = "indicators*\s*of\s*compromise", # 検索ワード
+    [int]$search_days_arg = 30, # 何日前まで取得するか指定, デフォルトは30日
+    [parameter(mandatory=$true)]$rss_list_arg # 調査rssのリスト(txt), 必須
+)
+
+$wc = New-Object System.Net.WebClient
+$wc.Encoding = [System.Text.Encoding]::UTF8
+
+function Find-Match{
+    # 日付の比較
+    If($search_days -le $date){
+        $wc_all = $wc.DownloadString($item.link.Trim())
+        if($wc_all | Select-String -Pattern $search_word){
+           "url:" + $item.link.Trim()
+           "title:" + $item.title
+            "----------------------------------------"
+        }
+    }
+}
+
+[String]$encoding='UTF-8'
+# 各rssを配列で格納
+[string[]]$rss_list = (Get-Content $rss_list_arg) -as [string[]]
+
+# $search_daysがデフォルトかどうか
+If([string]::IsNullOrEmpty($search_days_arg)){
+    # デフォルトの場合30日前に設定
+}else{
+    # 指定があった場合, 実行日時から計算して何日になるか計算
+    $search_days = (Get-Date).AddDays(-$search_days_arg)
+}
+
+"========================================"
+# rssリストのループ
+foreach($rss in $rss_list){
+    $rss = [xml]$wc.DownloadString($rss)
+    if($rss.rss -ne $null){ # rss=2.0
+        "blog_title:" + $rss.rss.channel.title + ""
+        "----------------------------------------"
+        foreach($item in $rss.rss.channel.item){
+            # 日付の比較
+            $date = [DateTime]$item.pubDate
+            Find-Match
+        }
+    }
+    else{ # rss=1.0
+        "blog_title:" + $rss.RDF.channel.title + ""
+        "----------------------------------------"
+        foreach($item in $rss.RDF.item){
+            # 日付の比較
+            $date = [DateTime]$item.date
+            Find-Match
+        }
+    }
+    "========================================"
+}
